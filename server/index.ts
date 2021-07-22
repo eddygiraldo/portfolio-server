1cd8fe20d3c07f34cd38d6b8ca3759923617f8ac
Eddy Giraldo <eddygiraldo@gmail.com>
2021-07-22 09:01:19 -0500

    feat(server): add swagger for documentation

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import swaggerUI from 'swagger-ui-express';
 import { portfolioApi } from './routes/portfolio';
+const swaggerDocument = require('./swagger.json');
 
 const PORT = 3000;
 const app = express();
 
 portfolioApi(app);
+
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(PORT, () => {
   console.log(`The application is listening on http://localhost:${PORT}`);
